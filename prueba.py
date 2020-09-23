4368257caed646c1b0ab815ea2239ab681687241
Chema-a <jose.davalos4980@alumnos.udg.mx>
2020-09-23 13:29:56 -0500

    Implementación de programa de prueba a Repl.it

---
+++ prueba.py
@@ -0,0 +1,62 @@
+mes = int(input("Dame el mes: "))
+dia = int(input("Dame el día: "))
+if mes == 1:
+    if dia < 21:
+        print("Eres capricornio")
+    else:
+        print("Eres acuario")
+elif mes == 2:
+    if dia < 20:
+        print(u"Eres acuario")
+    else:
+        print(u"Eres piscis")
+elif mes == 3:
+    if dia < 22:
+        print("Eres piscis")
+    else:
+        print("Eres aries")
+elif mes == 4:
+    if dia < 20:
+        print("Eres aries")
+    else:
+        print("Eres Tauro")
+elif mes == 5:
+    if dia < 21:
+        print("Eres Tauro")
+    else:
+        print("Eres Geminis")
+elif mes == 6:
+    if dia < 21:
+        print("Eres Geminis")
+    else:
+        print(u"Eres Cancer")
+elif mes == 7:
+    if dia < 23:
+        print("Eres Cancer")
+    else:
+        print("Eres Leo")
+elif mes == 8:
+    if dia < 24:
+        print("Eres Leo")
+    else:
+        print("Eres Virgo")
+elif mes == 9:
+    if dia < 24:
+        print("Eres Virgo")
+    else:
+        print("Eres Libra")
+elif mes == 10:
+    if dia < 24:
+        print("Eres Libra")
+    else:
+        print("Eres Escorpio")
+elif mes == 11:
+    if dia < 23:
+        print("Eres Escorpio")
+    else:
+        print("Eres Sagitario")
+elif mes == 12:
+    if dia < 23:
+        print(u"Eres Sagitario")
+    else:
+        print("Eres Capricornio")
